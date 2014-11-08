c518782d65ceda4b386180dccb2ff4036f5ba461
andrius@thingsilike <thingsilike.apphb@gmail.com>
2014-11-08 10:24:42 +0200

    Clock

--- AB.ThingsILike/AB.ThingsILike.Web/Scripts/Controllers.js
+++ AB.ThingsILike/AB.ThingsILike.Web/Scripts/Controllers.js
@@ -1,7 +1,19 @@
 ﻿var myApp = angular.module('myApp', []);
 
-var HomeController = function ($scope, Data) {
+myApp.controller('HomeController', function ($scope, Data) {
 
     $scope.data = Data;
 
-};
+});
+
+myApp.controller('ClockController', function ($scope) {
+
+    var updateClock = function () {
+        $scope.clock = new Date();
+    };
+
+    var timer = setInterval(function () {
+        $scope.$apply(updateClock);
+    }, 1000);
+    updateClock();
+});
