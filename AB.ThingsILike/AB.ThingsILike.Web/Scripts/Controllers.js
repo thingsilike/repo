8bc8d91aeebf51370834ce447de331ab9570e981
andrius@thingsilike <thingsilike.apphb@gmail.com>
2014-11-14 06:08:05 +0200

    Google analytics

--- AB.ThingsILike/AB.ThingsILike.Web/Scripts/Controllers.js
+++ AB.ThingsILike/AB.ThingsILike.Web/Scripts/Controllers.js
@@ -1,9 +1,15 @@
 ﻿
 
-myApp.controller('ListsController', function ($scope, $http) {
+myApp.controller('ListsController', function ($scope, $http, $location, $window) {
 
     $http.get('/api/Lists').success(function (dataReturned) {
         $scope.lists = dataReturned;
+    });
+
+    // Google analytics:
+
+    $scope.$on('$viewContentLoaded', function (event) {
+        $window.ga('send', 'pageview', { page: $location.path() });
     });
 
 });
